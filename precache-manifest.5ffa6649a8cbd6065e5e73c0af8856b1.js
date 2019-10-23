8076068f9fc8c4c36baf296ef1b33750e8e0a31c
Leo <leonardom1808@gmail.com>
2019-10-23 14:22:17 -0300

    Updates

---
+++ precache-manifest.5ffa6649a8cbd6065e5e73c0af8856b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79fa9a061fee1761c669dcc616e1b81",
+    "url": "/LoLStats/index.html"
+  },
+  {
+    "revision": "f3fb9aab3cce435d23d2",
+    "url": "/LoLStats/static/css/main.88ff4f15.chunk.css"
+  },
+  {
+    "revision": "c1880d20d65a6968482b",
+    "url": "/LoLStats/static/js/2.711e3c42.chunk.js"
+  },
+  {
+    "revision": "f3fb9aab3cce435d23d2",
+    "url": "/LoLStats/static/js/main.18f6f12e.chunk.js"
+  },
+  {
+    "revision": "e2c5bdf0f27e15c23179",
+    "url": "/LoLStats/static/js/runtime-main.3b5182ab.js"
+  }
+]);
